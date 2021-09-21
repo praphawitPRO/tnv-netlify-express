2da893da736c651ef967bb8de1bfcff6c84fa2ea
praphawitPRO <69111062+praphawitPRO@users.noreply.github.com>
2021-09-21 20:03:59 +0700

    add status

--- src/api.js
+++ src/api.js
@@ -71,7 +71,7 @@ router.get('/', function(req, res, next) {
           body.records = records ;
 
           axios.post("https://api.airtable.com/v0/appya8Wd8zuZbxvd0/%E0%B8%A3%E0%B8%B2%E0%B8%A2%E0%B8%81%E0%B8%B2%E0%B8%A3",body,{ headers: {"Authorization" : `Bearer ${API_KEY_AB}`, "Content-Type" : "application/json"} })
-                  .then(data => res.send("success"))
+                  .then(data => res.status(200).send("success"))
                   .catch(err => res.send(err));
         }
         catch(err){
